2df571c27d99449b1c3f8e0e5c68be8fca95185f
Vamshi-Akula <saivamshiakula63@gmail.com>
2025-09-08 09:08:54 +0530

    Added Product classes and data

---
+++ ecommerce/ecommerce/src/main/java/com/akula/ecommerce/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.akula.ecommerce.controller;
+
+
+import com.akula.ecommerce.model.Product;
+import com.akula.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+        @Autowired
+        private ProductService service;
+
+        @RequestMapping("/")
+        public String greet(){
+            return "Hello";
+        }
+
+        @GetMapping("/products")
+        public List<Product> getAllProducts(){
+            return service.getAllProducts();
+
+        }
+}
